57d373b3d7defa0ff8af61b4897008986a3d7f1b
unknown <cortanahp@gmail.com>
2022-04-17 14:17:59 +0530

    initial commit

---
+++ MadLibs.py
@@ -0,0 +1,7 @@
+color=input("Enter any color: ")
+name=input("enter a name: ")
+object=input("enter an object: git")
+print("Roses are "+ color+",")
+print("sky is blue,")
+print("My name is "+name+".")
+print("And you are "+object)
